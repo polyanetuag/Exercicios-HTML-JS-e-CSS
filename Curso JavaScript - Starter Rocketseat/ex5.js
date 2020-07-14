2f114547b6d9aba98c4fd9e41aaf54c8bcf7fdfb
Polyane P. Tuag <pollyportugues@hotmail.com>
2020-07-14 19:13:56 -0300

    Função com objeto dentro de uma array

---
+++ Curso JavaScript - Starter Rocketseat/ex5.js
@@ -0,0 +1,16 @@
+// Escreva uma função que percorra um objeto 
+
+var usuarios = [
+    {
+        nome: "Diego",
+        habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        nome: "Gabriel",
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+]
+
+console.log(`O ${usuarios[0].nome} possui as habilidades: ${usuarios[0].habilidades}`)
+
+console.log(`O ${usuarios[1].nome} possui as habilidades: ${usuarios[1].habilidades}` )
