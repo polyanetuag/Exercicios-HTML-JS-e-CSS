c10e207f558b3678f0224d8989cb316bacca49b0
Polyane P. Tuag <pollyportugues@hotmail.com>
2020-07-14 14:51:33 -0300

    Exerc. com Objeto

---
+++ Curso JavaScript/ex1.js
@@ -0,0 +1,9 @@
+var endereco = {
+    rua: 'Rua dos Pinheiros',
+    numero: 1293,
+    bairro: 'Centro',
+    cidade: 'São Paulo',
+    uf: 'SP'
+}
+
+console.log(`O usuário mora em ${endereco.cidade} / ${endereco.uf} ,no bairro ${endereco.bairro} , na ${endereco.rua} de número ${endereco.numero}`)
