885bb34d7d33ada7d3799a68fac666fb077cc930
Polyane P. Tuag <pollyportugues@hotmail.com>
2020-07-14 18:59:55 -0300

    Função com if else if

---
+++ Curso JavaScript - Starter Rocketseat/ex4.js
@@ -0,0 +1,22 @@
+// Escreva uma função que dado um total de anos de estudo, retorna o quão experiente o usuário é
+
+function experiencia(anos) {
+    if (anos <= 1) {
+        return 'Iniciante';
+    } else if (anos <=3) {
+        return 'Intermediário'
+    } else if (anos <=6) {
+        return 'Avançado'
+    } else {
+        return 'Jedi Master'
+    } 
+}
+
+var anosEstudo = experiencia(3)
+console.log(anosEstudo)
+
+
+// De 0-1 ano: Iniciante
+// De 1-3 anos: Intermediário
+// De 3-6 anos: Avançado
+// De 7 acima: Jedi Master
